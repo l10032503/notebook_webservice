d4dd02e23f5fbba737df00884de638e4b9e7b113
l100320503 <l10032503@gmail.com>
2018-11-13 18:20:01 +0900

    first commit

---
+++ src/main/java/com/example/notebookwebproject/NotebookwebprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.notebookwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookwebprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookwebprojectApplication.class, args);
+    }
+}
