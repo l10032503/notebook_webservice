dd7fe86e79cab8a71d66c2a10af15abdaf38c7ca
l100320503 <l10032503@gmail.com>
2018-12-26 19:48:18 +0900

    TEST

--- src/main/java/com/example/notebookwebproject/NoteBook/web/WebRestController.java
+++ src/main/java/com/example/notebookwebproject/NoteBook/web/WebRestController.java
@@ -1,18 +1,12 @@
-package com.example.notebookwebproject.NoteBook.web;
+package com.example.notebookwebproject;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 public class WebRestController {
-
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
     }
-
 }
