007511cb312d2bbe68ccb830111e5e748d7a122a
l100320503 <l10032503@gmail.com>
2018-12-26 17:53:56 +0900

    Posts 구현

---
+++ src/main/java/com/example/notebookwebproject/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.example.notebookwebproject.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
