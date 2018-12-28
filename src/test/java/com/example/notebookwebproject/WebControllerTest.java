049aaccf93b5466ad7be46d0eae63a48a501346a
l100320503 <l10032503@gmail.com>
2018-12-28 16:13:48 +0900

    profile test

--- src/test/java/com/example/notebookwebproject/WebControllerTest.java
+++ src/test/java/com/example/notebookwebproject/WebControllerTest.java
@@ -10,16 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class WebControllerTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void 메인페이지_로딩(){
-        
-    }
 
 }
--- src/test/java/com/example/notebookwebproject/WebRestControllerTest.java
+++ src/test/java/com/example/notebookwebproject/WebRestControllerTest.java
@@ -10,20 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class WebRestControllerTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void Profile확인 () {
-        //when
-        String profile = this.restTemplate.getForObject("/profile", String.class);
-
-        //then
-    }
 
 
 }
