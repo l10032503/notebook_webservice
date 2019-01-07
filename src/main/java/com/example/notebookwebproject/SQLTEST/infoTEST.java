df378f070957b6630d07f73ff30e11dd61608656
sook1515030 <sook1515030@gmail.com>
2019-01-07 20:04:44 +0900

    notebookinfo 표입니다

--- src/main/java/com/example/notebookwebproject/SQLTEST/infoTEST.java
+++ src/main/java/com/example/notebookwebproject/SQLTEST/infoTEST.java
@@ -102,4 +102,180 @@ public class infoTEST {
         return status;
     }
 
+
+
+    private int Resolution, HDDSize, SSDSize, AfterService;
+    private float Thick;
+    private String Brand, CPUCode, CPUNumber, CPUCore, MemoryType,
+            GPUManufact, GPUKind, SSD, OS;
+    private Boolean LanCable, LanRadio, Bluetooth, HDMITwo, HDMI, miniDP, DP, DSUB, WebCam,
+            USBTypeCTrd, USBTypeC, USBTrdOne, USBTrd, USBSnd, ThunderBolt, eSATA, IEEE, SerialPort, ExpressCard, MultiReader,
+            NumKB, BlockKB, MachineKB, RGBLight, KBLight, WaterProofKB, MILSTD, Face, Fingerprint, TPM;
+
+    //표1
+    public String getBrand(){
+        return Brand;
+    }
+    public String getWeightString(){
+        int Qweight = Weight;
+        String WeightString = "";
+        if(Qweight >= 1000)
+            WeightString = Qweight/1000+"kg";
+        else
+            WeightString = Qweight+"g";
+        return WeightString;
+    }
+    public float getThick(){
+        return Thick;
+    }
+    public String getResolution(){
+        String rstring = "";
+        if(Resolution == 1366)
+            rstring = "1366×768";
+        else if(Resolution == 1440)
+            rstring = "1440×900";
+        else if(Resolution == 1920)
+            rstring = "1920×1080";
+        else if(Resolution == 2304)
+            rstring = "2304×1440";
+        else if(Resolution == 2560)
+            if(Brand.equals("애플"))
+                rstring = "2560×1600";
+            else
+                rstring = "2560×1440";
+        else if(Resolution == 2880)
+            rstring = "2880×1800";
+        else if(Resolution == 3840)
+            rstring = "3840×2160";
+        return rstring;
+    }
+    public String getCPUCode(){
+        return CPUCode;
+    }
+    public String getCPUNumber(){
+        return CPUNumber;
+    }
+    public String getCPUCore(){
+        return CPUCore;
+    }
+
+    public int getMemorySize(){
+        return MemorySize;
+    }
+    public String getMemoryType(){
+        return MemoryType;
+    }
+    public String getGPUManufact(){
+        return GPUManufact;
+    }
+    public String getGPUKind(){
+        return GPUKind;
+    }
+    public String getHDDSize(){
+        String hddstring = "";
+        if(HDDSize !=0){
+            if(HDDSize >= 1024)
+                hddstring = HDDSize/1000 + "TB";
+            else
+                hddstring = HDDSize + "GB";}
+        return hddstring;
+    }
+    public String getSSD(){
+        return SSD;
+    }
+    public String getSSDSize(){
+        String ssdstring = "";
+        if(SSDSize !=0){
+            if(SSDSize >= 1024)
+                ssdstring = SSDSize/1000 + "TB";
+            else
+                ssdstring = SSDSize + "GB";}
+        return ssdstring;
+    }
+    public String getOS(){
+        return OS;
+    }
+    public String getAfterService(){
+        String asstring = "";
+        if(AfterService != 0)
+            asstring = AfterService+"년";
+        return asstring;
+    }
+
+    //표2
+    public String getNetworkString(){
+        Boolean lc, lr, bt;
+        String nstring = "";
+        lc = LanCable; lr = LanRadio; bt = Bluetooth;
+        if(lc) nstring += "유선랜 / ";
+        if(lr) nstring += "무선랜 / ";
+        if(bt) nstring += "블루투스 / ";
+        if (nstring.length() > 0)
+            nstring = nstring.substring(0, nstring.length()-3);
+        return nstring;
+    }
+    public String getVideoString(){
+        boolean ht, hd, mn, dp, ds, wc;
+        String nstring = "";
+        ht = HDMITwo; hd = HDMI; mn = miniDP; dp = DP; ds = DSUB; wc = WebCam;
+        if(ht) nstring += "HDMI 2.0 / ";
+        if(hd) nstring += "HDMI / ";
+        if(mn) nstring += "miniDP / ";
+        if(dp) nstring += "DP / ";
+        if(ds) nstring += "D-SUB / ";
+        if(wc) nstring += "웹캠 / ";
+        if (nstring.length() > 0)
+            nstring = nstring.substring(0, nstring.length()-3);
+        return nstring;
+    }
+    public String getTerminalString(){
+        Boolean usb1, usb2, usb3, usb4, usb5, tb, es, ie, sp, ec, mr;
+        String nstring = "";
+        usb1 = USBTypeCTrd; usb2 = USBTypeC; usb3 = USBTrdOne; usb4 = USBTrd; usb5 = USBSnd;
+        tb = ThunderBolt; es = eSATA; ie = IEEE; sp = SerialPort; ec = ExpressCard; mr = MultiReader;
+        if(usb1) nstring += "USB3.1 Type-C / ";
+        if(usb2) nstring += "USB Type-C / ";
+        if(usb3) nstring += "USB 3.1 / ";
+        if(usb4) nstring += "USB 3.0 / ";
+        if(usb5) nstring += "USB 2.0 / ";
+        if(tb) nstring += "썬더볼트 / ";
+        if(es) nstring += "e-SATA / ";
+        if(ie) nstring += "IEEE1394 / ";
+        if(sp) nstring += "시리얼포트 / ";
+        if(ec) nstring += "익스프레스카드 / ";
+        if(mr) nstring += "멀티리더기 / ";
+        if (nstring.length() > 0)
+            nstring = nstring.substring(0, nstring.length()-3);
+        return nstring;
+    }
+    public String getKeyboardString(){
+        Boolean nk, bk, mk, rl, kl, wp, mi;
+        String nstring = "";
+        nk = NumKB; bk = BlockKB; mk = MachineKB; rl = RGBLight; kl = KBLight; wp = WaterProofKB; mi = MILSTD;
+        if(nk) nstring += "숫자키패드 / ";
+        if(bk) nstring += "블록키보드 / ";
+        if(mk) nstring += "기계식키보드 / ";
+        if(rl) nstring += "RGB라이트 / ";
+        if(kl) nstring += "키보드라이트 / ";
+        if(wp) nstring += "침수지연키보드 / ";
+        if(mi) nstring += "MIL-STD / ";
+        if (nstring.length() > 0)
+            nstring = nstring.substring(0, nstring.length()-3);
+        return nstring;
+    }
+    public String getSecurityString(){
+        Boolean fc, fp, tpm;
+        String nstring = "";
+        fc = Face; fp = Fingerprint; tpm = TPM;
+        if(fc) nstring += "얼굴인식 / ";
+        if(fp) nstring += "지문인식 / ";
+        if(tpm) nstring += "TPM / ";
+        if (nstring.length() > 0)
+            nstring = nstring.substring(0, nstring.length()-3);
+        return nstring;
+    }
+
+
+
+
 }
--- src/main/java/com/example/notebookwebproject/web/InfoController.java
+++ src/main/java/com/example/notebookwebproject/web/InfoController.java
@@ -15,15 +15,47 @@ public class InfoController {
 
     private infoTESTMapper infoTestMapper;
 
+    @GetMapping("/about")
+    public String about(){
+        return "chatbot";
+    }
+
     @RequestMapping("/notebookinfo/{ID}")
     public String notebookinfo(@PathVariable int ID, final Model model){
         infoTEST infoTest = infoTestMapper.findinfotestID(ID);
         model.addAttribute("id", infoTest.getID());
         model.addAttribute("model", infoTest.getModel());
-        model.addAttribute("price", infoTest.getPrice());
-        model.addAttribute("weight", infoTest.getWeight());
         model.addAttribute("imgurlf", infoTest.getImgURLf());
         model.addAttribute("imgurlb", infoTest.getImgURLb());
+        model.addAttribute("brand", infoTest.getBrand());
+        model.addAttribute("price", infoTest.getPrice());
+        model.addAttribute("pricestring", infoTest.getPriceString());
+        model.addAttribute("sizeint", infoTest.getSizeINT());
+        model.addAttribute("weight", infoTest.getWeight());
+        model.addAttribute("weightstring", infoTest.getWeightString());
+        model.addAttribute("thick", infoTest.getThick());
+        model.addAttribute("resolution", infoTest.getResolution());
+        model.addAttribute("cpumanufact", infoTest.getCPUManufact());
+        model.addAttribute("cpukind", infoTest.getCPUKind());
+        model.addAttribute("cpucode", infoTest.getCPUCode());
+        model.addAttribute("cpunumber", infoTest.getCPUNumber());
+        model.addAttribute("cpucore", infoTest.getCPUCore());
+        //
+        model.addAttribute("memorytype", infoTest.getMemoryType());
+        model.addAttribute("memorysize", infoTest.getMemorySize());
+        model.addAttribute("gpumanufact", infoTest.getGPUManufact());
+        model.addAttribute("gpukind", infoTest.getGPUKind());
+        model.addAttribute("hddsize", infoTest.getHDDSize());
+        model.addAttribute("ssd", infoTest.getSSD());
+        model.addAttribute("ssdsize", infoTest.getSSDSize());
+        model.addAttribute("os", infoTest.getOS());
+        model.addAttribute("afterservice", infoTest.getAfterService());
+        //
+        model.addAttribute("networkstring", infoTest.getNetworkString());
+        model.addAttribute("videostring", infoTest.getVideoString());
+        model.addAttribute("terminalstring", infoTest.getTerminalString());
+        model.addAttribute("keyboardstring", infoTest.getKeyboardString());
+        model.addAttribute("securitystring", infoTest.getSecurityString());
         return "notebookinfo";
     }
 
