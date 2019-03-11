890bb0e7c87aef79293c8b160ab08815c8f83c78
l100320503 <l10032503@gmail.com>
2019-03-11 17:12:40 +0900

    서비스페이지 추가

---
+++ src/main/java/com/example/notebookwebproject/web/ServiceController.java
@@ -0,0 +1,21 @@
+package com.example.notebookwebproject.web;
+
+import com.example.notebookwebproject.SQLTEST.infoTEST;
+import com.example.notebookwebproject.SQLTEST.infoTESTMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+public class ServiceController {
+
+    @GetMapping("/service")
+    public String service(){
+        return "service";
+    }
+
+}
